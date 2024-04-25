2850f2ceee0a7bd082b3c7df8adc1294f229f039
Bhoami K Khona <bhoamikhona@gmail.com>
2024-04-25 23:44:29 +0530

    Home page complete - footer component complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <p className="footer__attribution">
+        Copyrights Reserved &copy;. Design and Code by{" "}
+        <a
+          className="footer__attribution-link"
+          href="https://github.com/bhoamikhona"
+        >
+          Bhoami K Khona
+        </a>
+        .
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
